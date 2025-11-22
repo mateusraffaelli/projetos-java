f3ec773c5da00c7403cc3da7cf8a23faf90c27c2
Mateus Raffaelli <mattewscastro@gmail.com>
2025-11-21 22:08:10 -0300

    Implementação no jogo do número aleatorio: Niveis de dificuldade

--- jogos/AdivinheNum.java
+++ jogos/AdivinheNum.java
@@ -1,3 +1,4 @@
+package jogos;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,18 +6,52 @@ public class AdivinheNum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int numeroAleatorio = random.nextInt(100) + 1;
-        System.out.println(numeroAleatorio);
         
         System.out.println("");
         System.out.println("=== JOGO DO NÚMERO ALEATÓRIO ===");
-        System.out.println("Escolha um número inteiro de 1 a 100:");
+        System.out.println("Escolha o nível de dificuldade:");
+        System.out.println("1. Fácil (1-50)");
+        System.out.println("2. Médio (1-100)");
+        System.out.println("3. Difícil (1-500)");
+        System.out.println("4. Impossível (1-1000)");
+        int dificuldade = scanner.nextInt();
+
+        int numeroAleatorio;
+        int numMax;
+
+        switch (dificuldade) {
+            case 1:
+                numMax = 50;
+                numeroAleatorio = random.nextInt(50) + 1;
+                break;
+            
+            case 2:
+                numMax = 100;
+                numeroAleatorio = random.nextInt(100) + 1;
+                break;
+                
+            case 3:
+                numMax = 500;
+                numeroAleatorio = random.nextInt(500) + 1;
+                break;
+
+            case 4:
+                numMax = 1000;
+                numeroAleatorio = random.nextInt(1000) + 1;
+                break;
+        
+            default:
+                System.out.println("Opção Inválida");
+                return;
+        }
+
+        System.out.println("Escolha um número inteiro de 1 a " + numMax);
         int tentativa = scanner.nextInt();
         
         int numTentativas = 1;
 
         while (tentativa != numeroAleatorio) {
-             if (tentativa < 1 || tentativa > 100) {
+             if (tentativa < 1 || tentativa > numMax) {
                 System.out.println("Número inválido.");
             }else{
                 if (tentativa > numeroAleatorio) {
@@ -27,7 +62,6 @@ public class AdivinheNum {
             }
             numTentativas++;
 
-            System.out.println("Digite um número:");
             tentativa = scanner.nextInt();
         }
 
@@ -36,7 +70,7 @@ public class AdivinheNum {
             System.out.println("Uau! Parabéns, você acertou o número de primeira");
         }else{
             System.out.println("Parabéns! Você acertou o número aleatório.");
-            System.out.println("Você acertou com " + numTentativas + " tentativas"); 
+            System.out.println("Você acertou em " + numTentativas + " tentativas"); 
         }
        
         System.out.println("");
